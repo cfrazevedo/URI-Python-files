d561ed224ce1beedac9514e744d733ea1aa9a9f2
cfrazevedo <cfrazevedo@gmail.com>
2021-05-20 14:20:15 -0300

    Exercícios de Python feitos no URI

---
+++ URI1186-AbaixoDaDiagonalSecundaria.py
@@ -0,0 +1,11 @@
+o = str(input()).upper()
+v = []
+for linha in range(0, 12):
+    for coluna in range(0, 12):
+        x = float(input())
+        if coluna + linha > 11:
+            v.append(x)
+if o == 'S':
+    print(f'{(sum(v)):.1f}')
+elif o == 'M':
+    print(f'{(sum(v) / len(v)):.1f}')
