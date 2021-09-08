21dbdee7399ff928f4386a08e1099b859c71eb15
cfrazevedo <cfrazevedo@gmail.com>
2021-09-08 16:23:11 -0300

    Exercícios de Python feitos no URI

---
+++ URI1012-Area.py
@@ -0,0 +1,10 @@
+linha1 = input().split()
+a = float(linha1[0])
+b = float(linha1[1])
+c = float(linha1[2])
+pi = 3.14159
+print(f'TRIANGULO: {(a * c / 2):.3f}')
+print(f'CIRCULO: {((c ** 2) * pi):.3f}')
+print(f'TRAPEZIO: {((a + b) * c / 2):.3f}')
+print(f'QUADRADO: {(b ** 2):.3f}')
+print(f'RETANGULO: {(a * b):.3f}')
