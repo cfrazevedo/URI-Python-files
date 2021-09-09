35aaf1af11fee9e6b69094039d70ab0054aae556
cfrazevedo <cfrazevedo@gmail.com>
2021-09-09 15:06:00 -0300

    Exercícios de Python feitos no URI

---
+++ URI1015-DistanciaEntreDoisPontos.py
@@ -0,0 +1,8 @@
+p1 = input().split()
+p2 = input().split()
+x1 = float(p1[0])
+y1 = float(p1[1])
+x2 = float(p2[0])
+y2 = float(p2[1])
+distancia = (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** (1 / 2)
+print(f'{distancia:.4f}')
