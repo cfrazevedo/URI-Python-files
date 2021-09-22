9335e6af34ffc935c3eae028c0e680e257a29942
cfrazevedo <cfrazevedo@gmail.com>
2021-09-22 11:32:14 -0300

    Exercícios de Python feitos no URI

---
+++ URI1049-Animal.py
@@ -0,0 +1,25 @@
+a = str(input())
+b = str(input())
+c = str(input())
+if a == 'vertebrado':
+    if b == 'ave':
+        if c == 'carnivoro':
+            print('aguia')
+        elif c == 'onivoro':
+            print('pomba')
+    elif b == 'mamifero':
+        if c == 'herbivoro':
+            print('vaca')
+        elif c == 'onivoro':
+            print('homem')
+if a == 'invertebrado':
+    if b == 'inseto':
+        if c == 'hematofago':
+            print('pulga')
+        elif c == 'herbivoro':
+            print('lagarta')
+    elif b == 'anelideo':
+        if c == 'hematofago':
+            print('sanguessuga')
+        elif c == 'onivoro':
+            print('minhoca')
