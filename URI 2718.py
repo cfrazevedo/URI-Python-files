810ac9fe12c80a9898baafbbf6b080a4f97dcbdf
cfrazevedo <cfrazevedo@gmail.com>
2021-10-06 12:19:29 -0300

    Exercícios de Python feitos no URI

---
+++ URI 2718.py
@@ -0,0 +1,12 @@
+n = int(input())
+for c in range(n):
+    x = bin(int(input()))[2:]
+    s = q = 0
+    for c in x:
+        if c == '1':
+            q += 1
+            if q > s:
+                s = q
+        if c == '0':
+            q = 0
+    print(s)
