7a1f3f98c057ce78850ddcca2d4f8a950c783e6d
cfrazevedo <cfrazevedo@gmail.com>
2021-10-07 14:32:01 -0300

    Exercícios de Python feitos no URI

--- URI 2715.py
+++ URI 2715.py
@@ -1,18 +1,26 @@
-# EXERCÍCIO NÃO RESOLVIDO
-# PROBLEMAS DE "TIME LIMIT EXCEEDED"
-# ACEITO SUGESTÕES
-
 while True:
     try:
         n = int(input())
         x = [int(z) for z in input().split()]
-        y = 0
-        for c in range(1, n):
-            d = abs(sum(x[:c]) - sum(x[c:]))
-            if c == 1 or d < y:
-                y = d
+        i = d = y = 0
+        c = (n + i) // 2
+        while True:
+            y = abs(sum(x[c:]) - sum(x[:c]))
+            r = sum(x[c:]) / sum(x[:c])
+            if r > 1:
+                i = c
+            elif r < 1:
+                n = c
             else:
                 break
+            c = (n + i) // 2
+            if d == c:
+                if y > abs(sum(x[c - 1:]) - sum(x[:c -1])):
+                    y = abs(sum(x[c - 1:]) - sum(x[:c - 1]))
+                elif y > abs(sum(x[c + 1:]) - sum(x[:c + 1])):
+                    y = abs(sum(x[c + 1:]) - sum(x[:c + 1]))
+                break
+            d = c
         print(y)
     except EOFError:
         break
