0b3f67bba2387a79c8a923cfbf4fe7afd91e047a
cfrazevedo <cfrazevedo@gmail.com>
2021-10-08 11:05:06 -0300

    Exercícios de Python feitos no URI

---
+++ URI 2748.py
@@ -0,0 +1,11 @@
+for c in range(7):
+    if c == 0 or c == 6:
+        print('-' * 39)
+    elif c == 1:
+        print('|' + (' ' * 8) + 'Roberto' + (' ' * 22) + '|')
+    elif c == 3:
+        print('|' + (' ' * 8) + '5786' + (' ' * 25) + '|')
+    elif c == 5:
+        print('|' + (' ' * 8) + 'UNIFEI' + (' ' * 23) + '|')
+    else:
+        print('|' + (' ' * 37) + '|')
