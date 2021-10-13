1d7bf10d800e1682d3eabc978644e716c45ca1a4
cfrazevedo <cfrazevedo@gmail.com>
2021-10-13 16:30:04 -0300

    Exercícios de Python feitos no URI

---
+++ URI 2754.py
@@ -0,0 +1,11 @@
+a = 234.345
+b = 45.698
+print('%.6f - %.6f' % (a, b))
+print('%.0f - %.0f' % (a, b))
+print('%.1f - %.1f' % (a, b))
+print('%.2f - %.2f' % (a, b))
+print('%.3f - %.3f' % (a, b))
+print('%e - %e' % (a, b))
+print('%E - %E' % (a, b))
+print('%g - %g' % (a, b))
+print('%g - %g' % (a, b))
