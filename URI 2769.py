110842ccc8ce738e03b1131999bf3b584c98ce41
cfrazevedo <cfrazevedo@gmail.com>
2021-11-03 21:45:54 -0300

    Exercícios de Python feitos no URI

--- URI 2769.py
+++ URI 2769.py
@@ -1,6 +1,3 @@
-# Questão Não Resolvida
-# Problema de Time Limit Error
-
 while True:
     try:
         n = int(input())
@@ -11,29 +8,23 @@ while True:
             t1 = [int(x) for x in input().split()]
             t2 = [int(x) for x in input().split()]
         x1, x2 = [int(x) for x in input().split()]
-        a1[0] += e1
-        a2[0] += e2
-        a1[len(a1) - 1] += x1
-        a2[len(a2) - 1] += x2
-        t = s = 0
-        for c in range(2 ** n):
-            p = f'{int(bin(s)[2:])}'.rjust(n, '0')
-            tx = 0
-            for d, e in enumerate(p):
-                if e == '0':
-                    tx += a1[d]
-                elif e == '1':
-                    tx += a2[d]
-                if n != 1:
-                    if d != 0:
-                        if e != p[d - 1]:
-                            if p[d - 1] == '0':
-                                tx += t1[d - 1]
-                            elif p[d - 1] == '1':
-                                tx += t2[d - 1]
-            if t == 0 or t > tx:
-                t = tx
-            s += 1
-        print(t)
+        d1 = d2 = 0
+        d1 += e1
+        d2 += e2
+        for i in range(n):
+            d1 += a1[i]
+            d2 += a2[i]
+            if n != 1 and i != n - 1:
+                if d1 + t1[i] < d2:
+                    d2 = d1 + t1[i]
+                elif d2 + t2[i] < d1:
+                    d1 = d2 + t2[i]
+        d1 += x1
+        d2 += x2
+        if d1 < d2:
+            print(d1)
+        else:
+            print(d2)
+
     except EOFError:
         break
