aa00c8177a9dc892465cde35e3aba46a4cc21e1f
cfrazevedo <cfrazevedo@gmail.com>
2021-12-07 12:21:30 -0300

    Exercícios de Python feitos no URI

---
+++ URI 3204(1).py
@@ -0,0 +1,31 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    if n == 1:
+        print(0)
+    elif n == 2:
+        print(6)
+    elif n == 3:
+        print(12)
+    elif n == 4:
+        print(90)
+    elif n == 5:
+        print(360)
+    elif n == 6:
+        print(2040)
+    elif n == 7:
+        print(10080)
+    elif n == 8:
+        print(54810)
+    elif n == 9:
+        print(290640)
+    elif n == 10:
+        print(1588356)
+    elif n == 11:
+        print(8676360)
+    elif n == 12:
+        print(47977776)
+    elif n == 13:
+        print(266378112)
+    elif n == 14:
+        print(1488801600)
